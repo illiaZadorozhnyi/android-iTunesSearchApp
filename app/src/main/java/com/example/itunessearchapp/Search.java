bb010d25c52cc5e52660feaa0faf805a8bc182e6
illiaZadorozhnyi <izadorozhnyi@vibrenthealth.com>
2019-04-10 22:35:13 -0400

    * initial project setup * added dependencies for retrofit and gson * created class for main values from response object

---
+++ app/src/main/java/com/example/itunessearchapp/Search.java
@@ -0,0 +1,51 @@
+package com.example.itunessearchapp;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Search {
+
+    private String artistName;
+
+    @SerializedName("trackName")
+    private String movieName;
+
+    private String artworkUrl60;
+    private double trackPrice;
+    private double trackRentalPrice;
+    private String primaryGenreName;
+    private String contentAdvisoryRating;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getArtworkUrl60() {
+        return artworkUrl60;
+    }
+
+    public double getTrackPrice() {
+        return trackPrice;
+    }
+
+    public double getTrackRentalPrice() {
+        return trackRentalPrice;
+    }
+
+    public String getPrimaryGenreName() {
+        return primaryGenreName;
+    }
+
+    public String getContentAdvisoryRating() {
+        return contentAdvisoryRating;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    private String longDescription;
+}
